0605533a699ef8a622ed8202804f5d62929f9588
smatsumori <shoya.matsumori@gmail.com>
2017-01-07 18:19:28 +0900

    MOD: Add DHCPDISCOVER resend function

--- mydhcpc.h
+++ mydhcpc.h
@@ -4,11 +4,12 @@
 #include "./packet.h"
 #include <assert.h>
 #include <sys/select.h>
+#include <unistd.h>
 
 /* Defines */
 #define MAX_NAME 20
 #define MAX_DESCRIPTION 100
-#define DHCP_SERV_IPADDR "192.168.1.101"
+#define DHCP_SERV_IPADDR "131.113.108.53"
 #define ERR_PROCESSING 10
 #define ERR_SENDTO 11
 #define ERR_SELECT 12
@@ -50,6 +51,8 @@ void init(struct dhcphead *hpr)
 	struct in_addr ipaddr;	// ipaddr for dhcp serv
 	socd = socket(PF_INET, SOCK_DGRAM, 0);		// get socket descriptor
 
+	fprintf(stderr, "Socket descriptor: %d\n", socd);
+
 	/* set server socket */
 	assert(inet_aton(DHCP_SERV_IPADDR, &ipaddr) == 1);	// set dhcp serv ip
 	fprintf(stderr, "DHCP server's IP has set to: %s\n", DHCP_SERV_IPADDR);
@@ -67,13 +70,15 @@ void init(struct dhcphead *hpr)
 /* Q02 */
 void send_discover(struct dhcphead *hpr)
 {
-	fprintf(stderr, "Send Discover\n");
+	fprintf(stderr, "Send DISCOVER\n");
 
 	struct dhcp_packet dpacket = {
 		.op = DHCP_DISCOVER, .siaddr = hpr->socaddptr->sin_addr, 
 		.siport = hpr->socaddptr->sin_port, .ciport = DHCP_CLI_PORT
 	};
 	
+	fprintf(stderr, "Seinding DISCOVER to: %d\n", hpr->mysocd);
+
 	int dsize;		/* rval is data size sent */
 	if ((dsize = sendto(hpr->mysocd, &dpacket, sizeof dpacket, 0,
 					(struct sockaddr *)(hpr->socaddptr), sizeof *(hpr->socaddptr))) < 0) {
@@ -82,6 +87,14 @@ void send_discover(struct dhcphead *hpr)
 	fprintf(stderr, "SENT: DISCOVER\n");
 	fprintf(stderr, "SIZE: %d\n", dsize);
 	
+	return;
+}
+
+void resend_discover(struct dhcphead *hpr)
+{
+	fprintf(stderr, "Resend DISCOVER in 5secs.\n");
+	sleep(5);
+	send_discover(hpr);
 	return;
 }
 
@@ -109,7 +122,8 @@ int recvoffer(struct dhcphead *hpr)
 	fd_set rdfds;		/* sets of file descriptor */
 	FD_ZERO(&rdfds);		/* set fds to 0 */
 	FD_SET(hpr->mysocd, &rdfds);		/* set file descriptor */
-	if ((rv = select(hpr->mysocd, &rdfds, NULL, NULL, &timeout)) < 0) {
+
+	if ((rv = select(hpr->mysocd + 1, &rdfds, NULL, NULL, &timeout)) < 0) {
 		report_error_and_exit(ERR_SELECT, "recvoffer");
 	} else if (rv == 0) {
 		fprintf(stderr, "Time out. No data after 5 secs.\n");
@@ -123,6 +137,7 @@ int recvoffer(struct dhcphead *hpr)
 			}
 		}
 	}
+
 }
 
 
