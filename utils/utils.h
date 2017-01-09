6e7151364c15966be43cf59a8aa6a9ef661ca99c
Shoya Matsumori <shoya.matsumori@gmail.com>
2017-01-09 14:58:03 +0900

    ADD: utils
    Inside utils we define some debug informations.

---
+++ utils/utils.h
@@ -0,0 +1,24 @@
+#ifndef __MYDHCPUTILS__
+#define __MYDHCPUTILS__
+
+
+/*** DEFINES ***/
+#define MAX_NAME 20
+#define MAX_DESCRIPTION 100
+#define MSG_TIMEOUT 10
+#define DHCP_SERV_IPADDR "131.113.108.53"
+
+/*** ERROR DEFINES ***/
+#define ERR_PROCESSING 10
+#define ERR_SENDTO 11
+#define ERR_SELECT 12
+#define ERR_RECVFROM 13
+#define ERROR_EVENT 14
+
+#define ERR_INVALID_OP 112
+#define ERR_INVALID_CODE 113
+
+#define ERR_READ_CONFIG 213
+#define ERR_MALLOC 214
+
+#endif
